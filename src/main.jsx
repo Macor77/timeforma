ce75fae51bdad324327ca36bbbe6e2a29e6a0ff5
Macor77 <97656743+Macor77@users.noreply.github.com>
2025-09-12 16:14:21 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
+// src/main.jsx
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from './ErrorBoundary.jsx';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <ErrorBoundary>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ErrorBoundary>
 );
